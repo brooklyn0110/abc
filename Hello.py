95260d6b3d02f942c00b4beeef442c68611abeae
brooklyn0110 <154819761+brooklyn0110@users.noreply.github.com>
2023-12-25 21:46:59 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,18 @@
+#Bai1
+print("hello world")
+
+#Bai2
+a="hello world"
+print(a)
+
+#Bai3
+b="hello"
+c=" "
+d="world"
+print(b+c+d)
+
+#Bai4
+def ngon():
+    print("hello world")
+    return;
+ngon()
